65530185e7fdb21b886e1e02271bc7081b4252b5
zhangdekai <664939067@qq.com>
2021-05-15 16:05:19 +0800

    添加Git命令行pushCode.sh代码

---
+++ RXSwift_Explore/脚本Shell/pushCode.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#  pushCode.sh
+#  RXSwift_Explore
+#
+#  Created by zhang dekai on 2021/5/15.
+#  Copyright © 2021 mr dk. All rights reserved.
+
+echo push code to remote
+
+parmMsg=$1
+
+function push() {
+
+    git add .
+    
+    echo "commit msg = $parmMsg";
+    
+    git commit -m"${parmMsg}"
+
+    git push .
+    
+    if [ "$?" != '0' ]
+    then
+        echo "push failed"
+        exit
+    fi
+
+}
+
+push
