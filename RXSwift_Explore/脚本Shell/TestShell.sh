58f48edd97bd3a43f2413da553ff79fa3aa9ee1a
zhangdekai <664939067@qq.com>
2021-05-15 12:07:28 +0800

    Shell 命令演练

--- RXSwift_Explore/脚本Shell/TestShell.sh
+++ RXSwift_Explore/脚本Shell/TestShell.sh
@@ -4,21 +4,29 @@
 #  Runner
 #
 #  Created by zhang dekai on 2021/5/14.
-#  
-# chmod +x ./TestShell.sh  开启sh执行权限
-# ./TestShell.sh  #执行脚本
-#
-#  /bin/sh test.sh  And /bin/php test.php : 直接运行解释器, 不需要在第一行指定解释器信息
-#
-#
 
-echo 'echo is print. hello shell world'  # 打印
+# 大段注释
+:<<’
+
+‘ 可以为EOF
+
+教程链接：https://www.runoob.com/linux/linux-shell.html
+
+ chmod +x ./TestShell.sh  开启sh执行权限
+ 
+ ./TestShell.sh  #执行脚本
+
+  /bin/sh test.sh  And /bin/php test.php : 直接运行解释器, 不需要在第一行指定解释器信息
+’
+
+echo 'echo is print. hello shell world ###################################11'  # 打印
 
 # 变量 PS：不能有空格 不能是bash 里的关键字；无标点符号；
+# 变量类型 局部变量 环境变量 shell变量
 yourName='cat'
 your_age='20'
-your_address1='jinan'
-_your2_address='beijing'
+your_address1="jinan + \"$yourName"!\" # 双引号可以添加变量+转义字符
+_your2_address="beijing + ${your_age}"
 readonly yourEmail='664939067@qq.com'
 
 
@@ -30,7 +38,71 @@ echo '修改变量'
 _your2_address='huang_he'
 echo _your2_address=$_your2_address
 
-unset _your2_address # 删除变量，不可删只读变量
+# 删除变量，不可删只读变量
+unset _your2_address
+
+# 获取字符串长度
+echo yourEmail len = ${#yourEmail}
+
+#字符串截取
+echo 截取 String = ${yourEmail:1:5} # 截取 String = 64939
+
+#查找字符串失败了呢？？？
+string="runoob is a great site"
+#echo `expr index "$string" io`  # 输出 4
+
+#数组 只支持 一维数组
+array_name=(value0 3345 value2 value3 7366 477384)
+array_name1=(
+value0
+value1
+value2
+value3
+)
+
+array_name[0]=1123
+
+number1=${array_name[0]}
+
+# 打印数组元素
+echo ${array_name[1]}  number1=$number1
+
+#数组长度
+echo array len = ${#array_name[*]}  array1 len=${#array_name[1]}
+echo "数组的元素为: ${array_name[*]}"
+echo "数组的元素为: ${array_name1[@]}"
+
+
+echo "Shell 传递参数实例！";
+echo "执行的文件名：$0";
+echo "第一个参数为：$1"
+echo "第二个参数为：$2"
+echo "第三个参数为：$3"
+echo "第四个参数为：$4";
+echo "参数们是 $#个 是 $*"
+echo 脚本运行的当前进程ID号是 $$
+echo 后台运行的最后一个进程的ID号 $!
+echo "参数们是 $#个 是 $@"
+
+# * 与 @ 的区别  " * " 等价于 "1 2 3"（传递了一个参数），而 "@" 等价于 "1" "2" "3"（传递了三个参数）。
+echo "-- \$* 演示 ---"
+for i in "$*"; do
+    echo $i
+done
+
+echo "-- \$@ 演示 ---"
+for i in "$@"; do
+    echo $i
+done
+
+for j in $*; do
+    echo $j
+done
+
+#  数字运算 expr ESC 按键下边 `expr 2 + 2`
+echo 数字运算
+val=`expr 2 + 2` #空格
+echo "两数之和为 : $val"
 
 echo '循环执行 for in ; do done'
 for ele in Cat dog house bed; do
---
+++ RXSwift_Explore/脚本Shell/testShell1.sh
@@ -0,0 +1,203 @@
+#!/bin/sh
+# 运算符 运算  https://www.runoob.com/linux/linux-shell-basic-operators.html
+a=10
+b=20
+
+val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
+
+val=`expr $b % $a`
+echo "b % a : $val"
+
+if [ $a == $b ]
+then
+   echo "a 等于 b"
+fi
+if [ $a != $b ]
+then
+   echo "a 不等于 b"
+fi
+
+# 关系运算符 -eq:==   -ne:!=  -gt:>  -lt:<   -ge:>=  -le:<=
+echo "\n关系运算符 -eq:==   -ne:!=  -gt:>  -lt:<   -ge:>=  -le:<= \n"
+echo [ $a -eq $b ]
+
+# ps: 注意空格
+if [ $a -eq $b ]
+then
+   echo "$a -eq $b : a 等于 b"
+else
+   echo "$a -eq $b: a 不等于 b"
+fi
+if [ $a -ne $b ]
+then
+   echo "$a -ne $b: a 不等于 b"
+else
+   echo "$a -ne $b : a 等于 b"
+fi
+if [ $a -gt $b ]
+then
+   echo "$a -gt $b: a 大于 b"
+else
+   echo "$a -gt $b: a 不大于 b"
+fi
+if [ $a -lt $b ]
+then
+   echo "$a -lt $b: a 小于 b"
+else
+   echo "$a -lt $b: a 不小于 b"
+fi
+if [ $a -ge $b ]
+then
+   echo "$a -ge $b: a 大于或等于 b"
+else
+   echo "$a -ge $b: a 小于 b"
+fi
+if [ $a -le $b ]
+then
+   echo "$a -le $b: a 小于或等于 b"
+else
+   echo "$a -le $b: a 大于 b"
+fi
+
+#bool运算  !  -a  -o   非  与  或
+echo "\n bool运算  !  -a  -o   非  与  或 \n"
+
+if [ $a != $b ]
+then
+   echo "$a != $b : a 不等于 b"
+else
+   echo "$a == $b: a 等于 b"
+fi
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+   echo "$a 小于 100 且 $b 大于 15 : 返回 true"
+else
+   echo "$a 小于 100 且 $b 大于 15 : 返回 false"
+fi
+if [ $a -lt 100 -o $b -gt 100 ]
+then
+   echo "$a 小于 100 或 $b 大于 100 : 返回 true"
+else
+   echo "$a 小于 100 或 $b 大于 100 : 返回 false"
+fi
+if [ $a -lt 5 -o $b -gt 100 ]
+then
+   echo "$a 小于 5 或 $b 大于 100 : 返回 true"
+else
+   echo "$a 小于 5 或 $b 大于 100 : 返回 false"
+fi
+
+#逻辑运算符 && ||
+echo "逻辑运算符 && ||"
+
+if [[ $a -lt 100 && $b -gt 100 ]]
+then
+   echo "返回 true"
+else
+   echo "返回 false"
+fi
+
+if [[ $a -lt 100 || $b -gt 100 ]]
+then
+   echo "返回 true"
+else
+   echo "返回 false"
+fi
+
+
+#字符串运算符
+
+echo "\n字符串运算符 = != -z -n "$"\n"
+
+a="abc"
+b="efg"
+
+if [ $a = $b ]
+then
+   echo "$a = $b : a 等于 b"
+else
+   echo "$a = $b: a 不等于 b"
+fi
+if [ $a != $b ]
+then
+   echo "$a != $b : a 不等于 b"
+else
+   echo "$a != $b: a 等于 b"
+fi
+if [ -z $a ]
+then
+   echo "-z $a : 字符串长度为 0"
+else
+   echo "-z $a : 字符串长度不为 0"
+fi
+if [ -n "$a" ]
+then
+   echo "-n $a : 字符串长度不为 0"
+else
+   echo "-n $a : 字符串长度为 0"
+fi
+if [ $a ]
+then
+   echo "$a : 字符串不为空"
+else
+   echo "$a : 字符串为空"
+fi
+
+#文件测试运算符
+
+echo "\n文件测试运算符\n"
+
+file="/Users/zhangdekai/Documents/GitHub/SwiftTest/RXSwift_Explore/脚本Shell/TestShell.sh"
+if [ -r $file ]
+then
+   echo "文件可读"
+else
+   echo "文件不可读"
+fi
+if [ -w $file ]
+then
+   echo "文件可写"
+else
+   echo "文件不可写"
+fi
+if [ -x $file ]
+then
+   echo "文件可执行"
+else
+   echo "文件不可执行"
+fi
+if [ -f $file ]
+then
+   echo "文件为普通文件"
+else
+   echo "文件为特殊文件"
+fi
+if [ -d $file ]
+then
+   echo "文件是个目录"
+else
+   echo "文件不是个目录"
+fi
+if [ -s $file ]
+then
+   echo "文件不为空"
+else
+   echo "文件为空"
+fi
+if [ -e $file ]
+then
+   echo "文件存在"
+else
+   echo "文件不存在"
+fi
+
