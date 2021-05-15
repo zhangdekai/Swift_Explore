03b82735db249c4eaead3f8a91501534e1fad40d
zhangdekai <664939067@qq.com>
2021-05-15 16:24:42 +0800

    pushAgainTest

--- RXSwift_Explore/脚本Shell/pushCode.sh
+++ RXSwift_Explore/脚本Shell/pushCode.sh
@@ -9,6 +9,25 @@
 echo push code to remote
 
 parmMsg=$1
+pushSuccess=0
+pushCount=0
+
+function pushAgain() {
+
+    while(( $pushCount<=5 && pushSuccess==0 ))
+    do
+        echo push again $pushCount 次
+        
+        git push
+        
+        if [ "$?" == '0']
+        then
+            echo push success
+            pushSuccess=1
+        fi
+    done
+    exit
+}
 
 function push() {
 
@@ -24,10 +43,15 @@ function push() {
     
     if [ "$?" != '0' ]
     then
-        echo "push failed"
+        echo "push failed!"
+        pushSuccess=0
+        pushAgain
+    else
+        echo "push success!"
         exit
     fi
-
 }
+
+
 
 push
